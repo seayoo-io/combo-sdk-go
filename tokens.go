b7a8db5cf9f634189d5b2a2e47503f06983c6e8c
Zhiguang Wang <wangzhiguang@kingsoft.com>
2023-12-22 20:31:21 +0800

    Add godoc to tokens.go

--- tokens.go
+++ tokens.go
@@ -14,10 +14,14 @@ const (
 	signingMethod = "HS256"
 )
 
+// TokenVerifier 用于验证世游服务端颁发的 Token。
 type TokenVerifier interface {
+	// VerifyIdentityToken 对 IdentityToken 进行验证，返回 IdentityPayload。
+	// 如果验证不通过，返回 error。
 	VerifyIdentityToken(idToken string) (*IdentityPayload, error)
 }
 
+// NewTokenVerifier 创建一个新的 TokenVerifier。
 func NewTokenVerifier(o Options) (TokenVerifier, error) {
 	if err := o.init(); err != nil {
 		return nil, err
@@ -33,12 +37,39 @@ func NewTokenVerifier(o Options) (TokenVerifier, error) {
 	}, nil
 }
 
+// IdentityPayload 包含了用户的身份信息。
 type IdentityPayload struct {
-	ExpiresAt     time.Time
-	ComboId       string
-	IdP           string
-	ExternalId    string
-	ExternalName  string
+	// ExpiresAt 是 IdentityToken 的过期时间。
+	ExpiresAt time.Time
+
+	// ComboId 是世游分配的聚合用户 ID。
+	// 游戏侧应当使用 ComboId 作为用户的唯一标识。
+	ComboId string
+
+	// IdP (Identity Provider) 是用户身份的提供者。
+	// 游戏侧可以使用 IdP 做业务辅助判断。
+	IdP string
+
+	// ExternalId 是用户在外部 IdP 中的唯一标识。
+	//
+	// 例如：
+	//
+	// - 如果用户使用世游通行证登录，那么 ExternalId 就是用户的世游通行证 ID。
+	//
+	// - 如果用户使用 Google Account 登录，那么 ExternalId 就是用户在 Google 中的账号标识。
+	//
+	// - 如果用户使用微信登录，那么 ExternalId 就是用户在微信中的 OpenId。
+	//
+	// 注意：游戏侧不应当使用 ExternalId 作为用户标识，但可以使用 ExternalId 做业务辅助判断。
+	ExternalId string
+
+	// ExternalName 是用户在外部 IdP 中的名称，通常是用户的昵称。
+	ExternalName string
+
+	// WeixinUnionid 是用户在微信中的 UnionId。
+	// 游戏侧可以使用 WeixinUnionid 实现多端互通。
+	//
+	// 注意：WeixinUnionid 只在 IdP 为 weixin 时才会有值。
 	WeixinUnionid string
 }
 
