6d9e6ea78a04ab02cd6507b1d5b9120bda0e02da
Zhiguang Wang <wangzhiguang@kingsoft.com>
2024-03-12 13:12:25 +0800

    Add server notification type refund

--- notifications.go
+++ notifications.go
@@ -12,6 +12,7 @@ import (
 
 const (
 	notificationType_ShipOrder = "ship_order"
+	notificationType_Refund    = "refund"
 )
 
 // NewNotificationHandler 创建一个用于接收世游服务端推送的通知的 http.Handler。
@@ -49,10 +50,44 @@ type NotificationListener interface {
 	// - 如果游戏内发货成功，则应当返回 nil。
 	// - 如果游戏内发货出现错误，则应当返回对应的 error。世游服务端会在稍后重试推送发货通知。
 	HandleShipOrder(ctx context.Context, id NotificationId, payload *ShipOrderNotification) error
+
+	// HandleRefund 用于处理订单退款通知。
+	// 目前仅支持对苹果 App Store 和谷歌 Play 商店的退款进行处理。
+	// 世游服务端会处理这些商店的退款通知，如果退款验证通过，则会向游戏侧推送退款通知。
+	// - 如果游戏内成功处理了退款通知，则应当返回 nil。
+	// - 如果游戏内处理退款时出现错误，则应当返回对应的 error。世游服务端会在稍后重试推送退款通知。
+	HandleRefund(ctx context.Context, id NotificationId, payload *RefundNotification) error
 }
 
-// ShipOrderNotification 是订单发货通知的数据结构，包含了订单的详细信息。
+// ShipOrderNotification 是订单发货通知的数据结构，包含了已支付订单的详细信息。
 type ShipOrderNotification struct {
+	// 世游服务端创建的,标识订单的唯一 ID。
+	OrderId string `json:"order_id"`
+
+	// 游戏侧用于标识创建订单请求的唯一 ID。
+	ReferenceId string `json:"reference_id"`
+
+	// 发起购买的用户的唯一标识。
+	ComboId string `json:"combo_id"`
+
+	// 购买的商品 ID。
+	ProductId string `json:"product_id"`
+
+	// 购买的商品的数量。
+	Quantity int `json:"quantity"`
+
+	// 订单币种代码。例如 USD CNY。
+	Currency string `json:"currency"`
+
+	// 订单金额,单位为分。
+	Amount int `json:"amount"`
+
+	// 游戏侧创建订单时提供的订单上下文，透传回游戏。
+	Context string `json:"context"`
+}
+
+// RefundNotification 是订单退款通知的数据结构，包含了被退款订单的详细信息。
+type RefundNotification struct {
 	// 世游服务端创建的,标识订单的唯一 ID。
 	OrderId string `json:"order_id"`
 
@@ -124,6 +159,16 @@ func (h *notificationHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 			return
 		}
 		if err := h.listener.HandleShipOrder(r.Context(), NotificationId(body.Id), &payload); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	case notificationType_Refund:
+		var payload RefundNotification
+		if err := json.Unmarshal(body.Data, &payload); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		if err := h.listener.HandleRefund(r.Context(), NotificationId(body.Id), &payload); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
